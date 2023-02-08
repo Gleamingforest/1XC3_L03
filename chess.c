f71c978a8960fe842b825d26ce9ec0ca7f5814b4
guo99 <guo99@pascal.cas.mcmaster.ca>
2023-02-08 18:51:07 -0500

    update3

--- chess.c
+++ chess.c
@@ -2,50 +2,33 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-void showBoard (int board[8][8]) {
+void showBoard (int board[8][9]) {
 	printf("\n\n╔════════╗\n");
 	for (int i = 0; i < 8; i++) {
 		printf("║"); 
-	printf("\n\n┌─┬─┬─┬─┬─┬─┬─┬─┐\n");
-	for (int i = 0; i < 8; i++) {
-//		printf("│"); 
-		bool flag = true;
 		for (int j = 0; j < 8; j++) {
-			if (!flag) {
-				printf("│"); 
-			}
-			flag = false;
-//			printf(""); 
-			switch (board[i][j]) {
-				case 0: 
+			if (board[i][j] == 0) {
+				if ((i + j) % 2 == 0) {
 					printf(" ");
-					break;
-				case 1:
-					printf("♔");
-					break;
-				case 2: 
-					printf("♕");
-					break;
-				case 3: 
-					printf("♗");
-					break;
-				case 4: 
-					printf("♘");
-					break;
-				case 5: 
-					printf("♙");
+				} else {
+					printf("▒");
+				}
+			} else if (board[i][j] == 1) {
+				printf("♔");
+ 			} else if (board[i][j] == 2) {
+				printf("♕");
+			} else if (board[i][j] == 3) {
+				printf("♖");
+			} else if (board[i][j] == 4) {
+				printf("♗");
+			} else if (board[i][j] == 5) {
+				printf("♘");
+			} else if (board[i][j] == 6) {
+				printf("♙");
 			}
 		}
-		printf("║\n") ;
-	}
-	printf("╚════════╝\n");
-		printf("│\n") ;
-		if (i < 7) {
-			printf("├─┼─┼─┼─┼─┼─┼─┼─┤\n") ;
-		}
-	}
-	printf("└─┴─┴─┴─┴─┴─┴─┴─┘\n");
-}
+		printf("║\n");
+	}printf("╚════════╝\n");}
 
 
 //bool memberOf (int *ValidMoves[2], int x, int y) ; 
@@ -59,54 +42,6 @@ void makeMove (char* input, int board[8][8]) {
 	board[end_x][end_y] = board[start_x][start_y];
 	board[start_x][start_y] = 0;
 }
-
-
-
-
-int main () {
-	printf("Welcome to Terminal Chess!\n");
-	printf("Initializing Board...\n")	;
-	int board [8][8] = { {5, 4, 3, 2, 1, 3, 4, 5}
-				             , {6, 6, 6, 6, 6, 6, 6, 6}
-				 	           , {0, 0, 0, 0, 0, 0, 0, 0}
-					           , {0, 0, 0, 0, 0, 0, 0, 0}
-				             , {0, 0, 0, 0, 0, 0, 0, 0} 
-				             , {0, 0, 0, 0, 0, 0, 0, 0}
-				             , {6, 6, 6, 6, 6, 6, 6, 6}
-				             , {5, 4, 3, 2, 1, 3, 4, 5}
-				             } ;
-  char buf[100];
-  bool whitesMove = true;
-	printf("Board Initialized!\n") ;
-	
-// 	showBoard(board);
-
-  printf("Enter `q` to quit at any time.\n");
-  printf("Moves are entered as co-ordinate pairs, such as \"13-33\" \n");
-  do {
-    bool flag = false; 
-    showBoard(board);
-    do {
-	if (flag) {
-		printf("Move Invalid! Try again!");
-	}
-    	if (whitesMove) {
-      		printf("White to move.\n");
-    	} else {
-      		printf("Black to move.\n");
-    	}
-    	printf("♔ >> ");
-	scanf("%s", buf);
-	flag = true;
-    } 
-	while (!isValidMove(buf, board) && buf[0] != 'q' && buf[0] != 'Q');
-    makeMove(buf, board);
-	whitesMove = !whitesMove;
-  } while (buf[0] != 'q' && buf[0] != 'Q') ;
-    
-	printf("Terminating...\n") ;
-}
-
 
 bool isValidMove (char* input, int board[8][8]) {
 	int start_y = input[0] - 48;
@@ -200,4 +135,50 @@ bool isValidMove (char* input, int board[8][8]) {
 	*/
 	return true;
 }
+
+
+
+int main () {
+	printf("Welcome to Terminal Chess!\n");
+	printf("Initializing Board...\n")	;
+	int board [8][8] = { {5, 4, 3, 2, 1, 3, 4, 5}
+				             , {6, 6, 6, 6, 6, 6, 6, 6}
+				 	         , {0, 0, 0, 0, 0, 0, 0, 0}
+					         , {0, 0, 0, 0, 0, 0, 0, 0}
+				             , {0, 0, 0, 0, 0, 0, 0, 0} 
+				             , {0, 0, 0, 0, 0, 0, 0, 0}
+				             , {6, 6, 6, 6, 6, 6, 6, 6}
+				             , {5, 4, 3, 2, 1, 3, 4, 5}
+				             } ;
+  char buf[100];
+  bool whitesMove = true;
+	printf("Board Initialized!\n") ;
+	
+  
+
+  printf("Enter `q` to quit at any time.\n");
+  printf("Moves are entered as co-ordinate pairs, such as \"13-33\" \n");
+  showBoard(board);
+  do {
+    bool flag = false;  
+    do {
+		if (flag) {
+		printf("Move Invalid! Try again!");
+	}
+    	if (whitesMove) {
+      		printf("White to move.\n");
+    	} else {
+      		printf("Black to move.\n");
+    	}
+    	printf("♔ >> ");
+	scanf("%s", buf);
+	flag = true;
+    }
+	while (!isValidMove(buf, board) && (buf[0] != 'q' )&& buf[0] != 'Q');
+    makeMove(buf, board);
+  } while (buf[0] != 'q' && buf[0] != 'Q') ;
+    
+	printf("Terminating...\n") ;
+}
+
 
