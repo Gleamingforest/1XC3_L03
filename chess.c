3db67fa32d6b6d6fc1417517b455a49666ee0a18
guo99 <guo99@pascal.cas.mcmaster.ca>
2023-02-08 20:04:14 -0500

    update3

--- chess.c
+++ chess.c
@@ -3,43 +3,46 @@
 #include <stdlib.h>
 
 void showBoard (int board[8][8]) {
-	printf("\n\n┌─┬─┬─┬─┬─┬─┬─┬─┐\n");
+  printf("\n 0  1 2 3 4 5 6 7");
+  printf("\n\n ╔════════════════╗\n");
 	for (int i = 0; i < 8; i++) {
-//		printf("│"); 
-		bool flag = true;
+    printf("%d║",i);
+    bool flag = true;
 		for (int j = 0; j < 8; j++) {
-			if (!flag) {
+      
+      if (!flag) {
 				printf("│"); 
 			}
 			flag = false;
-//			printf(""); 
-			switch (board[i][j]) {
-				case 0: 
+      printf(""); 
+			
+      if (board[i][j] == 0) {
+				if ((i + j) % 2 == 0) {
 					printf(" ");
-					break;
-				case 1:
-					printf("♔");
-					break;
-				case 2: 
-					printf("♕");
-					break;
-				case 3: 
-					printf("♗");
-					break;
-				case 4: 
-					printf("♘");
-					break;
-				case 5: 
-					printf("♙");
+				} else {
+					printf("▒");
+				}
+			} else if (board[i][j] == 1) {
+				printf("♔");
+ 			} else if (board[i][j] == 2) {
+				printf("♕");
+			} else if (board[i][j] == 3) {
+				printf("♗");
+			} else if (board[i][j] == 4) {
+				printf("♘");
+			} else if (board[i][j] == 5) {
+				printf("♖");
+			} else if (board[i][j] == 6) {
+				printf("♙");
 			}
 		}
-		printf("│\n") ;
+    printf(" ║\n");
 		if (i < 7) {
-			printf("├─┼─┼─┼─┼─┼─┼─┼─┤\n") ;
+			printf(" ╟─┼─┼─┼─┼─┼─┼─┼──╢\n") ;
 		}
-	}
-	printf("└─┴─┴─┴─┴─┴─┴─┴─┘\n");
 
+	}printf(" ╚════════════════╝\n");
+}
 
 //bool memberOf (int *ValidMoves[2], int x, int y) ; 
 
