440482d536352d84843da516864867e47567ae0c
guo99 <guo99@pascal.cas.mcmaster.ca>
2023-02-08 19:01:05 -0500

    update3

--- chess.c
+++ chess.c
@@ -2,33 +2,50 @@
 #include <stdbool.h>
 #include <stdlib.h>
 
-void showBoard (int board[8][9]) {
+void showBoard (int board[8][8]) {
 	printf("\n\n╔════════╗\n");
 	for (int i = 0; i < 8; i++) {
 		printf("║"); 
+	printf("\n\n┌─┬─┬─┬─┬─┬─┬─┬─┐\n");
+	for (int i = 0; i < 8; i++) {
+//		printf("│"); 
+		bool flag = true;
 		for (int j = 0; j < 8; j++) {
-			if (board[i][j] == 0) {
-				if ((i + j) % 2 == 0) {
+			if (!flag) {
+				printf("│"); 
+			}
+			flag = false;
+//			printf(""); 
+			switch (board[i][j]) {
+				case 0: 
 					printf(" ");
-				} else {
-					printf("▒");
-				}
-			} else if (board[i][j] == 1) {
-				printf("♔");
- 			} else if (board[i][j] == 2) {
-				printf("♕");
-			} else if (board[i][j] == 3) {
-				printf("♖");
-			} else if (board[i][j] == 4) {
-				printf("♗");
-			} else if (board[i][j] == 5) {
-				printf("♘");
-			} else if (board[i][j] == 6) {
-				printf("♙");
+					break;
+				case 1:
+					printf("♔");
+					break;
+				case 2: 
+					printf("♕");
+					break;
+				case 3: 
+					printf("♗");
+					break;
+				case 4: 
+					printf("♘");
+					break;
+				case 5: 
+					printf("♙");
 			}
 		}
-		printf("║\n");
-	}printf("╚════════╝\n");}
+		printf("║\n") ;
+	}
+	printf("╚════════╝\n");
+		printf("│\n") ;
+		if (i < 7) {
+			printf("├─┼─┼─┼─┼─┼─┼─┼─┤\n") ;
+		}
+	}
+	printf("└─┴─┴─┴─┴─┴─┴─┴─┘\n");
+}
 
 
 //bool memberOf (int *ValidMoves[2], int x, int y) ; 
@@ -142,13 +159,13 @@ int main () {
 	printf("Welcome to Terminal Chess!\n");
 	printf("Initializing Board...\n")	;
 	int board [8][8] = { {5, 4, 3, 2, 1, 3, 4, 5}
-				             , {6, 6, 6, 6, 6, 6, 6, 6}
-				 	         , {0, 0, 0, 0, 0, 0, 0, 0}
-					         , {0, 0, 0, 0, 0, 0, 0, 0}
-				             , {0, 0, 0, 0, 0, 0, 0, 0} 
-				             , {0, 0, 0, 0, 0, 0, 0, 0}
-				             , {6, 6, 6, 6, 6, 6, 6, 6}
-				             , {5, 4, 3, 2, 1, 3, 4, 5}
+				       , {6, 6, 6, 6, 6, 6, 6, 6}
+				 	   , {0, 0, 0, 0, 0, 0, 0, 0}
+					   , {0, 0, 0, 0, 0, 0, 0, 0}
+				       , {0, 0, 0, 0, 0, 0, 0, 0} 
+					   , {0, 0, 0, 0, 0, 0, 0, 0}
+					   , {6, 6, 6, 6, 6, 6, 6, 6}
+					   , {5, 4, 3, 2, 1, 3, 4, 5}
 				             } ;
   char buf[100];
   bool whitesMove = true;
